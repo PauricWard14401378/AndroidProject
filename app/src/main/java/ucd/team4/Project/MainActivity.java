a818c175a83bb716e6e6b36e3414c74a850f0d40
PauricWard14401378 <pauric.ward@ucdconnect.ie>
2017-11-29 22:05:53 +0000

    Four Buttons That link to a new Activity

--- app/src/main/java/ucd/team4/Project/MainActivity.java
+++ app/src/main/java/ucd/team4/Project/MainActivity.java
@@ -1,7 +1,10 @@
 package ucd.team4.Project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonOne = (Button) findViewById(R.id.button1);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                launchActivityRun(v);
+            }
+        });
+        Button buttonTwo = (Button) findViewById(R.id.button2);
+        buttonTwo.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                launchActivityRun(v);
+            }
+        });
+        Button buttonThree = (Button) findViewById(R.id.button3);
+        buttonThree.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                launchActivityRun(v);
+            }
+        });
+        Button buttonFour = (Button) findViewById(R.id.button4);
+        buttonFour.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                launchActivityRun(v);
+            }
+        });
     }
+    public void launchActivityRun(View v) {
+        Intent intent = new Intent(this, Maps.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/ucd/team4/Project/Maps.java
@@ -0,0 +1,16 @@
+package ucd.team4.Project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pauric on 29/11/2017.
+ */
+
+public class Maps  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
