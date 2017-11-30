e62b1bbb547618720e0546a947da7d096ad07d46
PauricWard14401378 <pauric.ward@ucdconnect.ie>
2017-11-30 10:27:42 +0000

    Trying out simple Pedometer

---
+++ app/src/main/java/ucd/team4/Project/RunHistory.java
@@ -0,0 +1,8 @@
+package ucd.team4.Project;
+
+/**
+ * Created by pauric on 30/11/2017.
+ */
+
+public class RunHistory {
+}
---
+++ app/src/main/java/ucd/team4/Project/StepListener.java
@@ -0,0 +1,11 @@
+package ucd.team4.Project;
+
+/**
+ * Created by pauric on 29/11/2017.
+ */
+// Will listen to step alerts
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
